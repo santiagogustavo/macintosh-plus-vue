4af060b7696159d342c74a3d3776fbdd6b1f7253
santiagogustavo <santiago.gustavo.mail@gmail.com>
2021-10-31 17:27:44 -0300

    chore(troisjs): init project

---
+++ .eslintrc.js
@@ -0,0 +1,11 @@
+module.exports = {
+  root: true,
+  env: {
+    browser: true,
+    node: true,
+  },
+  extends: ['plugin:vue/vue3-recommended', 'prettier'],
+  plugins: [],
+  // add your custom rules here
+  rules: {},
+};
---
+++ src/App.vue
@@ -0,0 +1,37 @@
+<template>
+  <Renderer
+    ref="renderer"
+    antialias
+    :orbit-ctrl="{ enableDamping: true }"
+    resize="window"
+  >
+    <Camera :position="{ z: 10 }" />
+    <Scene>
+      <PointLight :position="{ y: 50, z: 50 }" />
+      <Box ref="box" :rotation="{ y: Math.PI / 4, z: Math.PI / 4 }">
+        <LambertMaterial />
+      </Box>
+    </Scene>
+  </Renderer>
+</template>
+
+<script>
+export default {
+  mounted() {
+    const renderer = this.$refs.renderer;
+    const box = this.$refs.box.mesh;
+    renderer.onBeforeRender(() => {
+      box.rotation.x += 0.01;
+    });
+  },
+};
+</script>
+
+<style lang="scss">
+body {
+  margin: 0;
+}
+canvas {
+  display: block;
+}
+</style>
